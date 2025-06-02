dbfe8aabea9984644df214a89025dcfd70b78649
TaeWonM <minququ123456@gmail.com>
2025-06-02 21:16:25 +0900

    Week error revised and Add Email function

--- AI.sh
+++ AI.sh
@@ -2,3 +2,4 @@
 
 . ~/.bash_profile
 bash -c "source /docker/Newsis-Newsletter/newsis/bin/activate && python /docker/Newsis-Newsletter/AI/AI.py"
+bash -c "source /docker/Newsis-Newsletter/newsis/bin/activate && python /docker/Newsis-Newsletter/AI/Email.py"
