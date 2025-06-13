72ff5ece4732439c39795871bfcb1fe2ac7ea149
KyeongTaek <tommyholic@naver.com>
2025-06-13 14:50:43 +0900

    refactor: Remove unused app 'common'
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- WEB/Back-end/hn_crawling_experiment/settings.py
+++ WEB/Back-end/hn_crawling_experiment/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'corsheaders',
-    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- WEB/Back-end/hn_crawling_experiment/urls.py
+++ WEB/Back-end/hn_crawling_experiment/urls.py
@@ -23,8 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('common/', include('common.urls')),
-    path('api/', include('rest_framework.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
